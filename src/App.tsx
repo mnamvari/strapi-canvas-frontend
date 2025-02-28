8c5408a575f1df33f048983865029ccd359a4ffa
Mohammad Namvari <namvari@gmail.com>
2025-02-28 13:59:57 +0330

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import { DataProvider } from "@refinedev/strapi-v4";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import { authProvider, axiosInstance } from "./authProvider";
+import { API_URL } from "./constants";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <DevtoolsProvider>
+          <Refine
+            authProvider={authProvider}
+            dataProvider={DataProvider(API_URL + `/api`, axiosInstance)}
+            routerProvider={routerBindings}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              useNewQueryKeys: true,
+              projectId: "bbfHdX-PnE04n-rToIo0",
+            }}
+          >
+            <Routes>
+              <Route index element={<WelcomePage />} />
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
