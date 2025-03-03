f89145c9292e95002464ecf601fc8ba8feccb81c
Mohammad Namvari <namvari@gmail.com>
2025-03-03 16:56:14 +0330

    chore: ui improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { Refine } from "@refinedev/core";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Canvas from "./components/Canvas";
-import TestPage from "./pages/TestPage"; 
 import CanvasPage from "./pages/CanvasPage";
 
+
 const App = () => {
-  return (
+    return (
       <Router>
       <Refine>
-      <div className="p-6">
-        <h1 className="text-xl font-bold mb-4">Real-Time Canvas App12</h1>
+      <div className="p-6 h-screen bg-gray-50">
+        <h1 className="text-xl font-bold mb-4">Real-Time Canvas App</h1>
           <Routes>
               {/* Default route for the Canvas */}
               <Route path="/" element={<CanvasPage />} />
-
-              {/* New route for the TestPage */}
-              <Route path="/test" element={<TestPage />} />
           </Routes>
       </div>
     </Refine>
