ad8f514b864e14507720d2ad2dcd1e8c7e405b38
Mohammad Namvari <namvari@gmail.com>
2025-04-02 16:16:38 +0330

    chore: update backend base url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL = "https://automatic-sweltering-dog.strapiapp.com";
 export const TOKEN_KEY = "strapi-jwt-token";
 //export const BACKEND_STRAPI_BASE_URL = "http://localhost:1337";
-export const BACKEND_STRAPI_BASE_URL = "https://efficient-purpose-9f4af6d6fe.strapiapp.com/";
+export const BACKEND_STRAPI_BASE_URL = "https://realistic-dinosaur-9cb4e5ec36.strapiapp.com/";
