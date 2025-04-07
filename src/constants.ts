61eb255c53e1a160d79ee1c72f0e3da4f225ba7c
Mohammad Namvari <namvari@gmail.com>
2025-04-07 12:04:47 +0330

    chore: update API and frontend URLs to production environment

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
-export const API_URL = "http://localhost:1337";
-export const TOKEN_KEY = "strapi-jwt-token";
-export const BACKEND_STRAPI_BASE_URL = "http://localhost:1337";
-export const FRONTEND_URL = "http://localhost:5173";
+// export const API_URL = "http://localhost:1337";
+// export const TOKEN_KEY = "strapi-jwt-token";
+// export const BACKEND_STRAPI_BASE_URL = "http://localhost:1337";
+// export const FRONTEND_URL = "http://localhost:5173";
 
-// export const BACKEND_STRAPI_BASE_URL = "https://realistic-dinosaur-9cb4e5ec36.strapiapp.com/";
-//export const FRONTEND_URL = "https://strapi-real-time-canvas.vercel.app";
+export const API_URL = "https://realistic-dinosaur-9cb4e5ec36.strapiapp.com";
+export const TOKEN_KEY = "strapi-jwt-token";
+export const BACKEND_STRAPI_BASE_URL = "https://realistic-dinosaur-9cb4e5ec36.strapiapp.com";
+export const FRONTEND_URL = "https://strapi-real-time-canvas.vercel.app";
